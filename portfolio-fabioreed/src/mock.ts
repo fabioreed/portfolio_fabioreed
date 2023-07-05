033d0293aa359e16ca291f93f9a7fa96ba8b6010
Fabio Luiz <fabioeeereed@gmail.com>
2023-07-05 18:33:52 -0300

    add some backend projects

--- portfolio-fabioreed/src/mock.ts
+++ portfolio-fabioreed/src/mock.ts
@@ -75,6 +75,69 @@ const projects = [
     'technologies': ['HTML', 'JS', 'CSS'],
     'link': 'https://fabioreed.github.io/Maps/',
     'github': 'https://github.com/fabioreed/Maps'
+  },
+  {
+    'name': 'Market API',
+    'img': '',
+    'category': 'backend',
+    'description': "We received a request to create a REST API. This API will be used to organize the stock of a startup in the testing process that intends to become a network of autonomous markets (Smart Store). Today the market works with only two types of products: food and cleaning. As we will be controlling the market's stock, our application must have routes for creating, listing, updating and deleting products.",
+    'technologies': ['Typescript'],
+    'link': '',
+    'github': 'https://github.com/fabioreed/market-sp1-m4-fabioreed'
+  },
+  {
+    'name': 'Movies TypeORM API',
+    'img': '',
+    'category': 'backend',
+    'description': "The goal is to develop an API that will be used on a streaming platform. This API will be intended to manage the collection of available movies.",
+    'technologies': ['Typescript', 'PostgreSQL', 'TypeORM', 'Zod'],
+    'link': 'https://fabioreed.github.io/m2-api-empresas/',
+    'github': 'https://github.com/fabioreed/movies-com-typeorm'
+  },
+  {
+    'name': 'CRUD Admin API',
+    'img': '',
+    'category': 'backend',
+    'description': "The company you work for needs to create an MVP (Minimum Viable Product) of an API that controls users through a CRUD. This API also needs to have access control, where some resources can only be accessed by users who are logged in to the application, and other resources can only be accessed by users who are logged in and have administrator permissions.",
+    'technologies': ['Typescript', 'PostgresSQL'],
+    'link': '',
+    'github': 'https://github.com/fabioreed/crud_admin'
+  },
+  {
+    'name': 'Kimoveis - Real Estate',
+    'img': '',
+    'category': 'backend',
+    'description': "The owner of Kimóveis real estate hired his company to develop an application to manage his services. Through the application, it should be possible to register properties and users interested in acquiring properties. In addition, it should be possible to schedule and consult visit times to properties available in the real estate database.",
+    'technologies': ['Typescript', 'PostgresSQL', 'TypeORM', 'Zod'],
+    'link': '',
+    'github': 'https://github.com/fabioreed/crud_admin'
+  },
+  {
+    'name': 'PetKare - Python API',
+    'img': '',
+    'category': 'backend',
+    'description': "",
+    'technologies': ['Python', 'ORM', 'Django', 'Rest Framework'],
+    'link': '',
+    'github': 'https://github.com/fabioreed/pet-kare-fabioreed'
+  },
+  {
+    'name': 'Kenzie Buster - Python API',
+    'img': '',
+    'category': 'backend',
+    'description': "",
+    'technologies': ['Python', 'ORM', 'Django', 'Rest Framework'],
+    'link': '',
+    'github': 'https://github.com/fabioreed/m5-kenzie-buster-fabioreed'
+  },
+  {
+    'name': 'BandKamp - Python API',
+    'img': '',
+    'category': 'backend',
+    'description': "",
+    'technologies': ['Python', 'ORM', 'Django', 'Rest Framework'],
+    'link': '',
+    'github': 'https://github.com/fabioreed/bandkamp_api'
   }
 ]
 
